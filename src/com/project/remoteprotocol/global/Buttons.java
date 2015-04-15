7aa02594b2d42e40a2b03562ba683270310ce07c
sabin262@gmail.com <sabin262@gmail.com>
2015-04-15 06:59:59 +0545

    OK button mapped with enter

--- src/com/project/remoteprotocol/global/Buttons.java
+++ src/com/project/remoteprotocol/global/Buttons.java
@@ -13,6 +13,8 @@ public class Buttons {
 	public static final int KEY_END= KeyEvent.VK_ESCAPE;
 	public static final int KEY_TOGGLE_BLACK= KeyEvent.VK_B;
 	public static final int KEY_FULL_SCREEN= KeyEvent.VK_F5;
+	public static final int KEY_OK= KeyEvent.VK_ENTER;
+	
 	
 	public static final int MOUSE_BUTTON_LEFT = InputEvent.BUTTON1_DOWN_MASK;
 	public static final int MOUSE_BUTTON_RIGHT = InputEvent.BUTTON3_DOWN_MASK;
