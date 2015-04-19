4a931ee465fb6e34a8a0389dea4ba24c0c8932f9
sabin262@gmail.com <sabin262@gmail.com>
2015-04-19 14:28:10 +0545

    Joypad config

--- src/com/project/remoteprotocol/global/Buttons.java
+++ src/com/project/remoteprotocol/global/Buttons.java
@@ -3,6 +3,7 @@ package com.project.remoteprotocol.global;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.InputEvent;
+import java.sql.Struct;
 
 
 
@@ -30,9 +31,31 @@ public class Buttons {
 	public static final int KEY_VLC_VOLDOWN =  KeyEvent.VK_DOWN;
 	public static final int KEY_VLC_MUTE =  KeyEvent.VK_M;
 	public static final int KEY_VLC_FULLSCREEN =  KeyEvent.VK_F;
+
+
+	//controller1
+	public static final int JOY_PAD_UP=KeyEvent.VK_W;
+	public static final int JOY_PAD_DOWN=KeyEvent.VK_S;
+	public static final int JOY_PAD_RIGHT=KeyEvent.VK_D;
+	public static final int JOY_PAD_LEFT= KeyEvent.VK_A;
+	public static final int JOY_PAD_CROSS=KeyEvent.VK_J;
+	public static final int JOY_PAD_CIRCLE= KeyEvent.VK_K;
+	public static final int JOY_PAD_START= KeyEvent.VK_ENTER;
+	public static final int JOY_PAD_SELECT= KeyEvent.VK_ESCAPE;
+	public static final int JOY_PAD_TRIANGLE= KeyEvent.VK_U;
+	public static final int JOY_PAD_SQUARE=  KeyEvent.VK_H;
 	
-	
-	
+	//controller2
+	public static final int JOY_PAD_UP2=KeyEvent.VK_UP;
+	public static final int JOY_PAD_DOWN2=KeyEvent.VK_DOWN;
+	public static final int JOY_PAD_RIGHT2=KeyEvent.VK_RIGHT;
+	public static final int JOY_PAD_LEFT2= KeyEvent.VK_LEFT;
+	public static final int JOY_PAD_CROSS2=KeyEvent.VK_B;
+	public static final int JOY_PAD_CIRCLE2= KeyEvent.VK_N;
+	public static final int JOY_PAD_START2= KeyEvent.VK_M;
+	public static final int JOY_PAD_SELECT2= KeyEvent.VK_F;
+	public static final int JOY_PAD_TRIANGLE2= KeyEvent.VK_G;
+	public static final int JOY_PAD_SQUARE2=  KeyEvent.VK_V;
 	
 	
 }
